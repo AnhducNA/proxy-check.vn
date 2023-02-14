9394ab29907c9176a32d9295c95d203bc6166881
AnhducNA <Anhduc05022001>
2023-02-14 11:24:13 +0700

    check-proxy

--- src/ProxyCheckerCommand.php
+++ src/ProxyCheckerCommand.php
@@ -4,10 +4,6 @@ namespace Anhduc\ProxyCheck;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Pool;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Collection;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputDefinition;
@@ -45,50 +41,79 @@ class ProxyCheckerCommand extends Command
                     new InputOption('socks4', 's', null, 'Test socks4 proxies'),
                     new InputOption('socks5', 'S', null, 'Test socks5 proxies'),
                     new InputOption('http', 'H', null, 'Test http proxies'),
+                    new InputOption('all', 'a', null, 'Test all proto if no proto is specified in input file'),
+                    new InputOption('timeout', 'T', InputOption::VALUE_REQUIRED, 'Set timeout (default 3 seconds)'),
+                    new InputOption('url', 'u', InputOption::VALUE_REQUIRED, 'Url to test proxies'),
+                    new InputOption('proxies-file', 'f', InputOption::VALUE_REQUIRED, 'File to check proxy'),
+                    new InputOption('output-valid', 'o', InputOption::VALUE_REQUIRED, 'File to write valid proxies'),
+                    new InputOption('output-invalid', 'i', InputOption::VALUE_REQUIRED, 'File to write invalid proxies'),
                 ])
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $proxies = $this->loadProxiesFromFile('');
+        if ($input->getOption('socks4')) {
+            $protocol = 'socks4';
+        } else if ($input->getOption('socks5')) {
+            $protocol = 'socks5';
+        } else if ($input->getOption('http')) {
+            $protocol = 'http';
+        } else {
+            $protocol = '';
+        }
+        $timeout = ($input->getOption('timeout')) ? $input->getOption('timeout') : 3;
+        $uri = ($input->getOption('url')) ? $input->getOption('url') : 'https://ipinfo.io/json';
+        $urlGetProxies = ($input->getOption('proxies-file')) ? $input->getOption('proxies-file') : './proxy_list.txt';
+        $outputValidFile = ($input->getOption('output-valid')) ? $input->getOption('output_valid') : './proxy_valid.txt';
+        $outputInvalidFile = ($input->getOption('output-invalid')) ? $input->getOption('output_invalid') : './proxy_invalid.txt';
+        $fh1 = fopen($outputValidFile, 'a');
+        $fh2 = fopen($outputInvalidFile, 'a');
 
-        $client = new Client(['timeout' => 10]);
-        $uri = 'https://ipinfo.io/json';
 
-        $proxies->map(function (Proxy $proxy) use ($client, $uri, $output) {
-            $output->writeln($proxy->proxy);
-            try {
-                $client->request('GET', $uri, ['proxy' => $proxy->name]);
-                $proxy->is_available = true;
-            } catch (GuzzleException $e) {
-                $proxy->is_available = false;
+        $proxies = $this->loadProxiesFromFile($urlGetProxies);
+        $client = new Client(['timeout' => $timeout]);
+        $proxies->map(function (Proxy $proxy) use ($fh1, $fh2, $client, $uri, $output, $protocol) {
+            if ($protocol == $proxy->protocol) {
+                $output->writeln($proxy->proxy);
+                try {
+                    $client->request('GET', $uri, ['proxy' => $proxy->name]);
+                    $proxy->is_available = true;
+                    fwrite($fh2, $proxy->proxy . PHP_EOL);
+                } catch (GuzzleException $e) {
+                    $proxy->is_available = false;
+                    fwrite($fh1, $proxy->proxy . PHP_EOL);
+                }
+//                echo $proxy->protocol;
             }
         });
-
-        dd($proxies);
-
-        //save valid to file
-
-        //save invalid to file
+        fclose($fh1);
+        fclose($fh2);
     }
 
-    public function loadProxiesFromFile(string $path) {
-        $proxies = [
-            '23.254.100.211:8800',
-            '104.227.229.216:8800',
-            '23.254.10.184:8800',
-        ];
+    public function loadProxiesFromFile(string $path)
+    {
+        $file = file_get_contents($path);
+        $proxies = explode("\n", $file);
         $collection = new Collection([]);
         foreach ($proxies as $proxy) {
-            $proxy = new Proxy($proxy, 'http', $proxy);
+            if (str_contains($proxy, 'socks4')) {
+                $protocol = 'socks4';
+            } else if (str_contains($proxy, 'socks5')) {
+                $protocol = 'socks5';
+            } else if (str_contains($proxy, 'http')) {
+                $protocol = 'http';
+            } else {
+                $protocol = '';
+            }
+            $proxy = new Proxy($proxy, $protocol, $proxy);
             $collection->add($proxy);
         }
-
         return $collection;
     }
 
-    public function saveProxiesToFile(Collection $proxies, string $path) {
+    public function saveProxiesToFile(Collection $proxies, string $path)
+    {
 
     }
 }
